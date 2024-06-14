6ca93c8e92f67963418dba446cc24f3a83deab7e
Rui Matos <rui.microk@gmail.com>
2024-06-14 17:30:34 -0300

    aula07 ngDoCheck

---
+++ src/app/_components/exemplo-componente/exemplo-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-componente',
+  templateUrl: './exemplo-componente.component.html',
+  styleUrl: './exemplo-componente.component.css'
+})
+export class ExemploComponenteComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExemploComponenteComponent } from './_components/exemplo-componente/exemplo-componente.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExemploComponenteComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
